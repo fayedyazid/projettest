c75c95dafa0ecac6b859ff3791f386281cb94444
fayedyazid <fayedyazid1982@gmail.com>
2023-01-08 12:01:30 +0100

    add emplyee

---
+++ src/main/java/com/example/projetgithub/Employee.java
@@ -0,0 +1,2 @@
+package com.example.projetgithub;public class Employee {
+}
---
+++ src/main/java/com/example/projetgithub/ProjetgithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetgithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetgithubApplication.class, args);
+    }
+
+}
