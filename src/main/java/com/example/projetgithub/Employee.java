e392d82b0ba34cce27badbc37f6f9f7f7351d758
fayedyazid <fayedyazid1982@gmail.com>
2023-01-08 12:10:13 +0100

    update employee

--- src/main/java/com/example/projetgithub/Employee.java
+++ src/main/java/com/example/projetgithub/Employee.java
@@ -1,2 +1,22 @@
-package com.example.projetgithub;public class Employee {
+package com.example.projetgithub;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+private String name;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
